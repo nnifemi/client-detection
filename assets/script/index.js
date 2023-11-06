ac1390c3c7ac7c69b9f1a109278dfc07a0f7aa72
Nifemi Leye <147550833+nnifemi@users.noreply.github.com>
2023-11-06 13:27:08 -0600

    Update index.js

--- assets/script/index.js
+++ assets/script/index.js
@@ -7,14 +7,14 @@ function getBrowserName() {
 
     if (userAgent.indexOf("Firefox") !== -1) {
         return "Mozilla Firefox";
+    } else if (userAgent.indexOf("Opera") !== -1 || userAgent.indexOf("OPR") !== -1) {
+        return "Opera";
     } else if (userAgent.indexOf("Chrome") !== -1) {
         return "Google Chrome";
     } else if (userAgent.indexOf("Safari") !== -1) {
         return "Apple Safari";
     } else if (userAgent.indexOf("Edge") !== -1) {
         return "Microsoft Edge";
-    } else if (userAgent.indexOf("Opera") !== -1 || userAgent.indexOf("OPR") !== -1) {
-        return "Opera";
     } else if (userAgent.indexOf("Trident") !== -1) {
         return "Internet Explorer";
     } else {
